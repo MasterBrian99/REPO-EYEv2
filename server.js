ad4895b8db627ef1bb2e73eaaba3cc1138c02583
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2020-10-31 13:30:31 +0530

    add basic without templating

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
-  res.send("Hello");
+app.get("/", function(req, res) {
+    res.send("Hello");
 });
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+app.listen(3000, function() {
+    console.log("Server started on port 3000.");
 });
