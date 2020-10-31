a145066f2a5b14bdff49d95aa7618cb6d6759ae7
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2020-10-31 17:05:48 +0530

    add controllers and get api. render both pages

--- server.js
+++ server.js
@@ -2,11 +2,40 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
+var UserController = require('./controllers/UserController');
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+
 
 app.get("/", function(req, res) {
-    res.send("Hello");
+    res.render("pages/index", {
+        type: "repository"
+    });
+
+});
+
+app.post("/", function(req, res) {
+
+    console.log("post complete");
+
+});
+
+
+
+
+app.get("/users", function(req, res) {
+    res.render("pages/index", {
+        type: "user"
+    });
 });
 
 app.listen(3000, function() {
