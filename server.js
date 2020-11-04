418690a16d98f8f691a29f3432e520611f8c6875
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2020-11-04 21:15:58 +0530

    done.about file not finished

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const request = require("request");
 const app = express();
-const userController = require('./controllers/UserController');
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
@@ -16,34 +15,17 @@ app.use(express.static("public"));
 let info;
 let repoName;
 let usrInfo;
-let type;
-let avatar_url;
-let login;
-let html_url;
-let name;
-let bio;
-let followers;
-let following;
 let created_at;
-let dat;
-let updated_at;
 let lastLogin;
 
 
-
-
 app.get("/", function(req, res) {
-    res.render("pages/index", {
-        type: "repository"
-    });
-
+    res.render("pages/index");
 
 });
 
 app.post("/", function(req, res) {
-
     repoName = req.body.repoUrl;
-
     if (repoName == "") {
         res.redirect("/");
     } else {
@@ -67,26 +49,9 @@ app.post("/", function(req, res) {
                     }
                 };
 
-
                 function callbacks(error, response, body) {
                     if (!error && response.statusCode == 200) {
                         usrInfo = JSON.parse(body);
-
-                        type = usrInfo.type;
-                        avatar_url = usrInfo.avatar_url;
-                        login = usrInfo.login;
-                        html_url = usrInfo.html_url;
-                        name = usrInfo.name;
-                        bio = usrInfo.bio;
-                        followers = usrInfo.followers;
-                        following = usrInfo.following;
-
-                        dat = usrInfo.created_at.split("T");
-                        created_at = dat[0];
-
-                        lastLogin = usrInfo.updated_at.split("T");
-                        updated_at = lastLogin[0];
-
                         res.redirect("/info");
                     }
 
@@ -94,7 +59,6 @@ app.post("/", function(req, res) {
 
                 request(option, callbacks);
 
-                //      res.redirect("/info");
 
             } else {
                 console.log("failed");
@@ -102,8 +66,7 @@ app.post("/", function(req, res) {
             }
         };
 
-        var s = request(options, callback);
-        console.log(s);
+        request(options, callback);
 
     }
 
@@ -114,19 +77,47 @@ app.post("/", function(req, res) {
 
 app.get("/info", function(req, res) {
 
+    let dat = usrInfo.created_at.split("T");
+    created_at = dat[0];
 
+    lastLogin = usrInfo.updated_at.split("T");
+    let updated_at = lastLogin[0];
+    let repoSizeOriginal;
+
+    if (info.size < 1024) {
+        repoSizeOriginal = info.size + " KB";
+
+    } else {
+
+        repoSizeOriginal = Math.round(info.size / 1024).toFixed(1) + " MB";
+
+    }
 
     res.render("pages/info", {
-        type: type,
-        avatar_url: avatar_url,
-        login: login,
-        html_url: html_url,
-        name: name,
-        bio: bio,
-        followers: followers,
-        following: following,
+        type: usrInfo.type,
+        avatar_url: usrInfo.avatar_url,
+        login: usrInfo.login,
+        html_url: usrInfo.html_url,
+        name: usrInfo.name,
+        bio: usrInfo.bio,
+        followers: usrInfo.followers,
+        following: usrInfo.following,
         created_at: created_at,
-        updated_at: updated_at
+        updated_at: updated_at,
+        name: info.name,
+        default_branch: info.default_branch,
+        description: info.description,
+        language: info.language,
+        size: repoSizeOriginal,
+        forks_count: info.forks_count,
+        subscribers_count: info.subscribers_count,
+        open_issues_count: info.open_issues_count,
+        stargazers_count: info.stargazers_count,
+        git_url: info.git_url,
+        ssh_url: info.ssh_url,
+        clone_url: info.clone_url,
+        svn_url: info.svn_url
+
     });
 });
 
@@ -136,22 +127,3 @@ app.listen(3000, function() {
 
     console.log("Server started on port 3000.");
 });
-
-
-/*
-    const data = octokit.repos
-        .listForOrg({
-            org: "octokit",
-            type: "public",
-        })
-        .then(({
-            data
-        }) => {
-            console.log(data);
-            // handle data
-        });
-
-
-
-
-*/
